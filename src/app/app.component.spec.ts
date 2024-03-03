9b6ddb3259f90f182753edf9f1a8eea436ed1c32
rahuljain79 <jainrahul7225@gmail.com>
2024-03-03 17:14:46 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'restrowebnew' title`, () => {
+  it(`should have the 'restrop' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('restrowebnew');
+    expect(app.title).toEqual('restrop');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, restrowebnew');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, restrop');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FooterComponent } from './pages/footer/footer.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { CommonModule } from '@angular/common';
+import { ContactComponent } from './pages/contact/contact.component';
+import { nopageComponent } from './pages/404/404.component';
+import { BlogComponent } from './pages/blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FooterComponent,HeaderComponent,  CommonModule,ContactComponent,nopageComponent,BlogComponent,
+    RouterModule,],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'restrowebnew';
+  title = 'restrop';
 }
